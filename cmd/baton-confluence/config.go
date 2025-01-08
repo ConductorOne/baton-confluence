a0d3cf608f34d237ff056646a36bc3eec9bd80fa
Dan Larkin <dan@danlarkin.org>
2025-01-08 15:53:15 -0800

    allow nouns and verbs from the command line
    If these args are missing, then the defaults will be used.

--- cmd/baton-confluence/config.go
+++ cmd/baton-confluence/config.go
@@ -25,6 +25,16 @@ var (
 		field.WithDescription("Skip syncing personal spaces and their permissions"),
 		field.WithRequired(false),
 	)
+        nounsField = field.StringArrayField(
+		"noun",
+		field.WithDescription("The nouns for your Confluence Space sync"),
+		field.WithRequired(false),
+	)
+        verbsField = field.StringArrayField(
+		"verb",
+		field.WithDescription("The verbs for your Confluence Space sync"),
+		field.WithRequired(false),
+	)
 )
 
 var configuration = field.NewConfiguration(
@@ -33,5 +43,7 @@ var configuration = field.NewConfiguration(
 		domainUrl,
 		usernameField,
 		skipPersonalSpaces,
+		nounsField,
+		verbsField,
 	},
 )
